f3ae8b87af5bb96a3c789d91f3a81aa71d4c5a6c
Matt-Kinsey <mattkinsey_96@hotmail.com>
2018-03-29 14:46:58 -0400

    Search form added

---
+++ FinalProject_203/SearchForm.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SearchForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripLabel3 = New System.Windows.Forms.ToolStripLabel()
+        Me.cboUsers = New System.Windows.Forms.ToolStripComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dtpBegin = New System.Windows.Forms.DateTimePicker()
+        Me.dtpEnd = New System.Windows.Forms.DateTimePicker()
+        Me.lstEquipment = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnReserve = New System.Windows.Forms.Button()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.ToolStripLabel2 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabel1, Me.ToolStripLabel2, Me.ToolStripSeparator1, Me.ToolStripSeparator2, Me.ToolStripLabel3, Me.cboUsers})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(549, 25)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripLabel3
+        '
+        Me.ToolStripLabel3.Name = "ToolStripLabel3"
+        Me.ToolStripLabel3.Size = New System.Drawing.Size(33, 22)
+        Me.ToolStripLabel3.Text = "User:"
+        '
+        'cboUsers
+        '
+        Me.cboUsers.Name = "cboUsers"
+        Me.cboUsers.Size = New System.Drawing.Size(121, 25)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(47, 82)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Beginning:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(282, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "End:"
+        '
+        'dtpBegin
+        '
+        Me.dtpBegin.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpBegin.Location = New System.Drawing.Point(110, 76)
+        Me.dtpBegin.Name = "dtpBegin"
+        Me.dtpBegin.Size = New System.Drawing.Size(98, 20)
+        Me.dtpBegin.TabIndex = 3
+        '
+        'dtpEnd
+        '
+        Me.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpEnd.Location = New System.Drawing.Point(337, 76)
+        Me.dtpEnd.Name = "dtpEnd"
+        Me.dtpEnd.Size = New System.Drawing.Size(98, 20)
+        Me.dtpEnd.TabIndex = 4
+        '
+        'lstEquipment
+        '
+        Me.lstEquipment.FormattingEnabled = True
+        Me.lstEquipment.Location = New System.Drawing.Point(110, 154)
+        Me.lstEquipment.Name = "lstEquipment"
+        Me.lstEquipment.Size = New System.Drawing.Size(288, 225)
+        Me.lstEquipment.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(110, 135)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Available Equipment:"
+        '
+        'btnReserve
+        '
+        Me.btnReserve.Location = New System.Drawing.Point(220, 398)
+        Me.btnReserve.Name = "btnReserve"
+        Me.btnReserve.Size = New System.Drawing.Size(75, 23)
+        Me.btnReserve.TabIndex = 7
+        Me.btnReserve.Text = "Reserve"
+        Me.btnReserve.UseVisualStyleBackColor = True
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(86, 22)
+        Me.ToolStripLabel1.Text = "&Reservations"
+        '
+        'ToolStripLabel2
+        '
+        Me.ToolStripLabel2.Name = "ToolStripLabel2"
+        Me.ToolStripLabel2.Size = New System.Drawing.Size(78, 22)
+        Me.ToolStripLabel2.Text = "&Equipment"
+        '
+        'SearchForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(549, 464)
+        Me.Controls.Add(Me.btnReserve)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lstEquipment)
+        Me.Controls.Add(Me.dtpEnd)
+        Me.Controls.Add(Me.dtpBegin)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "SearchForm"
+        Me.Text = "Search"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents ToolStripLabel3 As ToolStripLabel
+    Friend WithEvents cboUsers As ToolStripComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents dtpBegin As DateTimePicker
+    Friend WithEvents dtpEnd As DateTimePicker
+    Friend WithEvents lstEquipment As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnReserve As Button
+    Friend WithEvents ToolStripLabel1 As ToolStripDropDownButton
+    Friend WithEvents ToolStripLabel2 As ToolStripDropDownButton
+End Class
---
+++ FinalProject_203/SearchForm.vb
@@ -0,0 +1,9 @@
+﻿Option Strict On
+Option Explicit On
+
+Public Class SearchForm
+    Private Sub SearchForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dtpBegin.MinDate = Date.Now
+        dtpEnd.MinDate = Date.Now
+    End Sub
+End Class
