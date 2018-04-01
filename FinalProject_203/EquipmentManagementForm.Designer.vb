c2ac0e6c4111585cab11cfc84ad2d5870704375c
Matt-Kinsey <mattkinsey_96@hotmail.com>
2018-04-01 09:42:51 -0400

    Equipment Management form

---
+++ FinalProject_203/EquipmentManagementForm.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EquipmentManagementForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtEquipmentName = New System.Windows.Forms.TextBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name:"
+        '
+        'txtEquipmentName
+        '
+        Me.txtEquipmentName.Location = New System.Drawing.Point(69, 38)
+        Me.txtEquipmentName.Name = "txtEquipmentName"
+        Me.txtEquipmentName.Size = New System.Drawing.Size(192, 20)
+        Me.txtEquipmentName.TabIndex = 1
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(67, 84)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(123, 23)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "Save Changes"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'EquipmentManagementForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(273, 144)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.txtEquipmentName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "EquipmentManagementForm"
+        Me.Text = "Equipment Management"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtEquipmentName As TextBox
+    Friend WithEvents btnSave As Button
+End Class
