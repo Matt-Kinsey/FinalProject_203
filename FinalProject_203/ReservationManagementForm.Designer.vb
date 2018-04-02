55da0ff40672f4670b057ea3438982d2832a62dc
Matt-Kinsey <mattkinsey_96@hotmail.com>
2018-04-02 06:44:12 -0400

    Reservation management form

---
+++ FinalProject_203/ReservationManagementForm.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ReservationManagementForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dtpBegin = New System.Windows.Forms.DateTimePicker()
+        Me.dtpEnd = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cboUsername = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtEquipment = New System.Windows.Forms.TextBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'dtpBegin
+        '
+        Me.dtpBegin.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpBegin.Location = New System.Drawing.Point(132, 22)
+        Me.dtpBegin.Name = "dtpBegin"
+        Me.dtpBegin.Size = New System.Drawing.Size(121, 20)
+        Me.dtpBegin.TabIndex = 4
+        '
+        'dtpEnd
+        '
+        Me.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpEnd.Location = New System.Drawing.Point(132, 65)
+        Me.dtpEnd.Name = "dtpEnd"
+        Me.dtpEnd.Size = New System.Drawing.Size(121, 20)
+        Me.dtpEnd.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Start date:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "End date:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 107)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Username:"
+        '
+        'cboUsername
+        '
+        Me.cboUsername.FormattingEnabled = True
+        Me.cboUsername.Location = New System.Drawing.Point(132, 104)
+        Me.cboUsername.Name = "cboUsername"
+        Me.cboUsername.Size = New System.Drawing.Size(121, 21)
+        Me.cboUsername.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(37, 149)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Equipment:"
+        '
+        'txtEquipment
+        '
+        Me.txtEquipment.Location = New System.Drawing.Point(132, 146)
+        Me.txtEquipment.Name = "txtEquipment"
+        Me.txtEquipment.ReadOnly = True
+        Me.txtEquipment.Size = New System.Drawing.Size(121, 20)
+        Me.txtEquipment.TabIndex = 11
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(89, 198)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(101, 23)
+        Me.btnSave.TabIndex = 12
+        Me.btnSave.Text = "Save Changes"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'ReservationManagementForm
+        '
+        Me.AcceptButton = Me.btnSave
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(274, 253)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.txtEquipment)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cboUsername)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dtpEnd)
+        Me.Controls.Add(Me.dtpBegin)
+        Me.Name = "ReservationManagementForm"
+        Me.Text = "Reservation Management"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dtpBegin As DateTimePicker
+    Friend WithEvents dtpEnd As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cboUsername As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtEquipment As TextBox
+    Friend WithEvents btnSave As Button
+End Class
