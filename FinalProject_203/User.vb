a7b1fee85d5ffe603c3a8dfeeeecdb153ebe8dbf
Matt-Kinsey <mattkinsey_96@hotmail.com>
2018-04-16 11:01:20 -0400

    Fixed Database (Userid was string instead of int), added user class

---
+++ FinalProject_203/User.vb
@@ -0,0 +1,28 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class User
+    Public ReadOnly Property ID As Integer
+    Public ReadOnly Property UserName As String
+    Public ReadOnly Property FullName As String
+    Public ReadOnly Property SSN As Integer
+
+    Public Sub New(ByVal pID As Integer, ByVal pUsername As String, ByVal pfullname As String, ByVal pSSN As Integer)
+        ID = pID
+        UserName = pUsername
+        FullName = pfullname
+        SSN = pSSN
+    End Sub
+
+    Public Sub New(ByRef u As User)
+        'Copy constructor
+        ID = u.ID
+        UserName = u.UserName
+        FullName = u.FullName
+        SSN = u.SSN
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return UserName
+    End Function
+End Class
